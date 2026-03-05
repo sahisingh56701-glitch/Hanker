c52e7a0248a07082be96fd2c4d4716ef523d3699
sahisingh56701-glitch <sahisingh56701@gmail.com>
2026-03-05 21:30:07 +0530

    Update config.py

--- PyToday/config.py
+++ PyToday/config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 # MongoDB Configuration
-MONGODB_URI = os.getenv("MONGODB_URI", "mongodb+srv://Aadish:aadish2008@ac-owiguby.cdly6hj.mongodb.net/?retryWrites=true&w=majority")
+MONGODB_URI = os.getenv("MONGODB_URI", "")
 
 # Bot Configuration
 BOT_TOKEN = os.getenv("BOT_TOKEN", "8773484109:AAF90W81bXSADrWGgiZgvA49bupo3KSIcg0")
